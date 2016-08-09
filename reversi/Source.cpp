75072d7a81b0a1953d5e70ff2d5d39c3181939d8
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2016-08-09 10:36:05 +0900

    add AI

--- reversi/Source.cpp
+++ reversi/Source.cpp
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<iostream>
-#include<string.h>
 using namespace std;
 
 class Point{
@@ -45,15 +44,56 @@ Point::Point(int xv, int yv){
 Point::Point(){}
 Point::~Point(){}
 
+bool reverse_alldirec(Point, bool);
+
+class reversi_AI{
+public:
+	reversi_AI();
+	~reversi_AI();
+	Point static think(){
+		int rating[10][10] = {{},{0,30,-12,0,-1},
+		{0,-12,-15,-3,-3},
+		{0,0,-3,0,-1},
+		{0,-1,-3,-1,-1}};
+		for(int y = 1; y <= 8; ++y){
+			for(int x = 1; x <= 8; ++x){
+				rating[y][9 - x] = rating[y][x];
+				rating[9 - y][x] = rating[y][x];
+				rating[9 - y][9 - x] = rating[y][x];
+			}
+		}
+
+
+		Point max_pos;
+		int max = -1000;//HACK:
+		for(int y = 1; y <= 8; ++y){
+			for(int x = 1; x <= 8; ++x){
+				if(reverse_alldirec(Point(x, y), true)){
+					if(rating[y][x] > max){
+						max = rating[y][x];
+						max_pos = Point(x, y);
+					}
+				}
+			}
+		}
+
+		return max_pos;
+	}
+
+private:
+};
+reversi_AI::reversi_AI(){}
+reversi_AI::~reversi_AI(){}
+
 enum stonecolor{
 	green,
 	black,
 	white,
 };
 stonecolor board[10][10];
-stonecolor turn;
+stonecolor current_turn;
 void init(){
-	turn = black;
+	current_turn = black;
 	for(int y = 0; y < 10; ++y){
 		for(int x = 0; x < 10; ++x){
 			board[y][x] = green;
@@ -64,7 +104,7 @@ void init(){
 	board[5][4] = black;
 	board[5][5] = white;
 }
-stonecolor get_enemy(stonecolor a){
+stonecolor get_enemy_color(stonecolor a){
 	if(a == black){
 		return white;
 	} else{
@@ -74,68 +114,89 @@ stonecolor get_enemy(stonecolor a){
 void draw(){
 	cout << "  ";
 	for(int x = 1; x <= 8; ++x){
-		cout << x;
+		cout << x << "|";
 	}
 	cout << endl;
 	for(int y = 1; y <= 8; ++y){
 		cout << y << " ";
 		for(int x = 1; x <= 8; ++x){
-			if(board[y][x] == green){
-				cout << " ";
-			} else{
-				cout << board[y][x];
+			switch(board[y][x]){
+				case white:
+					cout << "0|";
+					break;
+				case black:
+					cout << "$|";
+					break;
+				default:
+					cout << " |";
+					break;
 			}
 		}
 		cout << endl;
 	}
 }
-void show_score(){
-	int white_score = 0;
-	int black_score = 0;
+int get_score(stonecolor color){
+	int score = 0;
 	for(int y = 1; y <= 8; ++y){
 		for(int x = 1; x <= 8; ++x){
-			switch(board[y][x]){
-				case white:
-					white_score++;
-					break;
-				case black:
-					black_score++;
-					break;
-				default:
-					break;
+			if(board[y][x] == color){
+				++score;
 			}
 		}
 	}
-	cout << "黒" << black_score << " " << "白" << white_score << endl;
+	return score;
 }
-void change(){
-	if(turn == black){
-		turn = white;
+void show_score(){
+	cout << "$:" << get_score(black) << " " << "0:" << get_score(white) << endl;
+}
+void show_winner(){
+	if(get_score(black) > get_score(white)){
+		cout << "$の勝ち" << endl;
+	} else if(get_score(white) > get_score(black)){
+		cout << "0の勝ち" << endl;
 	} else{
-		turn = black;
+		cout << "引き分け" << endl;
+	}
+}
+void show_turn(){
+	switch(current_turn){
+		case black:
+			cout << "$のturn" << endl;
+			break;
+		case white:
+			cout << "0のturn" << endl;
+			break;
+		default:
+			break;
+	}
+}
+void change_player(){
+	if(current_turn == black){
+		current_turn = white;
+	} else{
+		current_turn = black;
 	}
 }
 
+//TODO:search_sand_pairとreverseまとめれそうじゃない?
+
 Point search_sand_pair(Point start_pos, Point direc){
-	//本当にこれでコピーされる?
 	Point search_pos = start_pos + direc;
-	if(board[search_pos.y][search_pos.x] == turn || board[search_pos.y][search_pos.x] == green){
+	if(board[search_pos.y][search_pos.x] == current_turn || board[search_pos.y][search_pos.x] == green){
 		return start_pos;
-		//returnでいいのか?
-		//相手がいなかった事を検索開始位置を返すことで示す
+		//とれなかった事を検索開始位置を返すことで示す
 	}
 
 	do{
 		search_pos += direc;
 		if(board[search_pos.y][search_pos.x] == green){
 			return start_pos;
-			//returnでいいのか?
 			//相手がいなかった事を検索開始位置を返すことで示す
 		}
-	} while(board[search_pos.y][search_pos.x] == get_enemy(turn));
+	} while(board[search_pos.y][search_pos.x] == get_enemy_color(current_turn));
 	return search_pos - direc;//ゴールの一歩手前までが裏返す所!
 }
-bool reverse(Point start_pos, Point direc){
+bool reverse(Point start_pos, Point direc, bool just_check){
 	Point reverse_pos = start_pos;
 	Point goal = search_sand_pair(start_pos, direc);
 	if(goal == start_pos){
@@ -143,16 +204,27 @@ bool reverse(Point start_pos, Point direc){
 	}
 	do{
 		reverse_pos += direc;
-		board[reverse_pos.y][reverse_pos.x] = turn;
+		if(!just_check){
+			board[reverse_pos.y][reverse_pos.x] = current_turn;
+		}
 	} while(!(reverse_pos == goal));
 	return true;
 }
-bool reverse_alldirec(Point fing){
+
+bool reverse_alldirec(Point fing, bool just_check){
+	//入力範囲チェック
+	if(!((1 <= fing.x && fing.x <= 8) && (1 <= fing.y && fing.y <= 8))){
+		return false;
+	}
+	//入力チェック既においてあるか．もうちょっと良い方法あるかも．
+	if(!(board[fing.y][fing.x] == green)){
+		return false;
+	}
 	bool ok = false;
 	for(int y = -1; y <= 1; ++y){
 		for(int x = -1; x <= 1; ++x){
 			if(!(y == 0 && x == 0)){
-				if(reverse(fing, Point(x, y))){
+				if(reverse(fing, Point(x, y), just_check)){
 					ok = true;
 				}
 			}
@@ -160,24 +232,63 @@ bool reverse_alldirec(Point fing){
 	}
 	return ok;
 }
-int main(){
-	init();
-	while(true){
-		draw();
-		show_score();
-		cout << turn << " turn" << endl;
 
-		Point fing_pos;
-		cin >> fing_pos.x;
-		cin >> fing_pos.y;
-		//スペース無しで入力出来ると便利
-
-		if(reverse_alldirec(fing_pos)){
-			board[fing_pos.y][fing_pos.x] = turn;
-			change();
-		} else{
-			cout << "そこにはおけません!!!!!" << endl;
+bool is_need_pass(stonecolor player){
+	for(int y = 1; y <= 8; ++y){
+		for(int x = 1; x <= 8; ++x){
+			if(reverse_alldirec(Point(x, y), true)){
+				return false;
+			}
 		}
+	}
+	return true;
+}
+bool is_end(){
+	if(is_need_pass(white) && is_need_pass(black)){//UNDONE:turnで今の状態が決まっちゃってるからblackだの指定しても無駄．
+		return true;
+	}
+	return false;
+}
+
+int main(){
+	while(true){
+		init();
+		cout << "先手は?" << endl;
+		int first_player;
+		//HACK:数値で受けとっちゃってる
+		cin >> first_player;
+		while(true){
+			draw();
+			if(is_end()){
+				break;
+			}
+			if(is_need_pass(current_turn)){
+				cout << "パス" << endl;
+				change_player();
+			}
+			show_turn();
+			Point fing_pos;
+			if(current_turn == first_player){
+				//HACK:ちょっぴりHACK
+				int temp;
+				cin >> temp;
+				fing_pos.x = temp / 10;
+				fing_pos.y = temp % 10;
+			} else{
+				fing_pos = reversi_AI::think();
+				cout << fing_pos.x << " " << fing_pos.y << "に置いたよ" << endl << endl;
+			}
+			if(reverse_alldirec(fing_pos, false)){
+				board[fing_pos.y][fing_pos.x] = current_turn;
+				change_player();
+			} else{
+				cout << "[[[そこにはおけません!!!!!]]]" << endl;
+			}
+			show_score();
+		}
+		cout << "終了" << endl;
+		cout << endl;
+		show_winner();
 	}
 	return 0;
 }
